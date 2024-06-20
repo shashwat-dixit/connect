ad9a21c22260d3c3193cd31bc57fd4c9e7575258
Shashwat Dixit <shashwatmain@gmail.com>
2024-06-20 23:52:24 +0530

    Init

---
+++ backend/index.js
@@ -0,0 +1,12 @@
+import express from "express"
+
+const port = 5000
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send("Hello, World!")
+})
+
+app.listen(port, () => {
+    console.log("Server started on PORT 5000")
+})
