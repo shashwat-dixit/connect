810ee2da8afbb4fca72a9c6d6eacfed2ad0b13f7
Shashwat Dixit <shashwatmain@gmail.com>
2024-07-03 04:35:26 +0530

    Fixed typescript issues

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,12 @@
-import express, { Request, Response } from 'express';
+import express, { Express, Request, Response } from 'express';
 
-const app = express();
+const app: Express = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req: Request, res: Response) => {
-    res.send('Hello, TypeScript with Express!');
+  res.send('Express + TypeScript Server');
 });
 
 app.listen(port, () => {
-    console.log(`Server is running at http://localhost:${port}`);
+  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
 });
