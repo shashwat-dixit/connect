5f505d889f4d8a97fab611d1ef5bd3dbd2e116dd
Shashwat Dixit <shashwatmain@gmail.com>
2024-07-14 01:39:42 +0530

    Added google-fonts and fixed the prop-types error

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,8 +3,8 @@
 function App() {
 
   return (
-    <div className="flex justify-center items-center w-full h-screen">
-      <h1 className="text-5xl">Hello, this is Connect!</h1>
+    <div className="flex flex-col justify-center items-center w-full h-screen">
+      <h1 className="text-5xl font-rubik mb-10">Hello, this is Connect!</h1>
     </div>
   )
 }
